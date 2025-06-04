3881ac634b80286904d81c0b2432eb2ab29c0e56
zhh <nkuzjh@gmail.com>
2025-06-04 13:08:27 +0000

    debug t2i code

--- lavis/models/blip2_models/blip2_qformer.py
+++ lavis/models/blip2_models/blip2_qformer.py
@@ -687,7 +687,7 @@ class Blip2Qformer(Blip2Base):
         score_i2t = compute_i2t_sim_matrix(model=self, data_loader=data_loader, k_test=k_test)
         return score_i2t
 
-    def compute_t2i_sim_matrix(self, data_loader, task_cfg, optimizer):
+    def compute_t2i_sim_matrix(self, data_loader, task_cfg):
         """
         Compute similarity i2t, t2i matrix for the given data loader.
         """
